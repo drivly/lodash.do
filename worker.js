ad023c900efc7fe024cd6f20eb14fe09bbe37f5e
Samuel Lippert <lippertz@gmail.com>
2022-08-29 10:19:18 -0500

    Add fetch passthrough

--- worker.js
+++ worker.js
@@ -2,12 +2,15 @@ import _ from 'lodash'
 
 export default {
   fetch: async (req) => {
-    const { pathname, search } = new URL(req.url)
-    const [__, operations, callback] = pathname.split('/')
+    let { pathname, search, protocol } = new URL(req.url)
+    let p = pathname.substring(1).split('/').reverse()
+    const operation = p.pop()
+    pathname = pathname.substring(operation.length + 1)
+    console.log({ operation, pathname, search, url: protocol + '/' + pathname + search })
 
-    console.log({ __, operations, callback })
+    const data = await fetch(protocol + '/' + pathname + search, req).then((res) => res.json())
 
-    const retval = {}
+    const retval = { data }
 
     return new Response(JSON.stringify(retval, null, 2), { headers: { 'content-type': 'application/json' } })
   },
